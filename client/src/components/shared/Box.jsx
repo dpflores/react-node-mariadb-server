5659def6af24381747d8b78eec06c7ac45ee996f
acme <acme@acmecia.com>
2023-12-21 10:15:33 -0500

    u

--- client/src/components/shared/Box.jsx
+++ client/src/components/shared/Box.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-export default function Box({ children, flex=1 }) {
+export default function Box({ children, flex = 1 }) {
   return (
-    <div className={`bg-white p-4 rounded-sm border border-solid border-gray-200 flex flex-col flex-${flex}`}>
+    <div
+      className={`bg-white p-4 rounded-sm border border-solid border-gray-200 flex flex-col flex-${flex}`}
+    >
       {children}
     </div>
   );
